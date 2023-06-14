cc990c46c1b94cbbb9b0c7879ac48d883a2009b8
minji0608 <88191880+minjiLee68@users.noreply.github.com>
2023-06-14 16:02:47 +0900

    Initial Commit

---
+++ CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by 이민지 on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
